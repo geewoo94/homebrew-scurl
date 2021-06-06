d546652df9565b6e7ca99491e3e38564a5fbdc92
gyeongwoopark <geewoo94@gmail.com>
2021-06-06 14:53:22 +0900

    fix formular

--- scurl.rb
+++ scurl.rb
@@ -1,6 +1,6 @@
 class Scurl < Formula
-  desc "use curl command with history"
-  homepage "https://github.com/geewoo94/scurl"
+  desc ""
+  homepage ""
   url "https://github.com/geewoo94/scurl/archive/refs/tags/v1.0.0.tar.gz"
   version "v1.0.0"
   sha256 "0fc6eabee5807c339d83d7b7d373b1083cd4879d7c1dfc1c7131e580d42a0d8a"
