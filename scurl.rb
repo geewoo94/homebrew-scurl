fce33ddf5de85000f7c472dfcf7d58843b6306fc
gyeongwoopark <geewoo94@gmail.com>
2021-06-06 14:27:39 +0900

    add formular

---
+++ scurl.rb
@@ -0,0 +1,12 @@
+class Scurl < Formula
+  desc "use curl command with history"
+  homepage "https://github.com/geewoo94/scurl"
+  url "https://github.com/geewoo94/scurl/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0fc6eabee5807c339d83d7b7d373b1083cd4879d7c1dfc1c7131e580d42a0d8a"
+  license ""
+
+  def install
+	bin.install "scurl" 
+  end
+
+end
