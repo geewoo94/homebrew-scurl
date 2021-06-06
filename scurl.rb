99ac9ee150731e1d645caaac0f99b025a1966f80
Geewoo94 <geewoo94@gmail.com>
2021-06-06 14:58:51 +0900

    Update scurl.rb

--- scurl.rb
+++ scurl.rb
@@ -1,8 +1,8 @@
 class Scurl < Formula
   desc ""
   homepage ""
-  url "https://github.com/geewoo94/scurl/archive/refs/tags/v1.0.0.tar.gz"
-  version "v1.0.0"
+  url "https://github.com/geewoo94/scurl/archive/refs/tags/v1.0.1.tar.gz"
+  version "v1.0.1"
   sha256 "0fc6eabee5807c339d83d7b7d373b1083cd4879d7c1dfc1c7131e580d42a0d8a"
 
   def install
