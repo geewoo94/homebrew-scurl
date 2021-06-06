550fd0edfc70d9dd1772da354e85391d81794080
gyeongwoopark <geewoo94@gmail.com>
2021-06-06 14:35:13 +0900

    add version

--- scurl.rb
+++ scurl.rb
@@ -2,8 +2,8 @@ class Scurl < Formula
   desc "use curl command with history"
   homepage "https://github.com/geewoo94/scurl"
   url "https://github.com/geewoo94/scurl/archive/refs/tags/v1.0.0.tar.gz"
+  version "v1.0.0"
   sha256 "0fc6eabee5807c339d83d7b7d373b1083cd4879d7c1dfc1c7131e580d42a0d8a"
-  license ""
 
   def install
 	bin.install "scurl" 
